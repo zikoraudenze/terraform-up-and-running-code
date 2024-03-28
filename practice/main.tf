a469db1089d3b6b50092983e0f6d21cba1b187ea
zikoraudenze <zikoraudenze@gmail.com>
2024-03-27 22:36:35 -0500

    add user data script

--- practice/main.tf
+++ practice/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
