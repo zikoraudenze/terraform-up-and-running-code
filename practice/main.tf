178b1db4547c82a8db47cdc4ebafc2ef7f655bdd
zikoraudenze <zikoraudenze@gmail.com>
2024-03-27 20:21:29 -0500

    add user data script

--- practice/main.tf
+++ practice/main.tf
@@ -2,6 +2,14 @@ resource "aws_instance" "exxample" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
 
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+                
+    user_data_replace_change = true
+
     tags = {
         Name = "terraform-example"
     }   
