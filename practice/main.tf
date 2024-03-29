dfbc49cbadb8578e6156ac9cdf34cd8972a293c4
zikoraudenze <zikoraudenze@gmail.com>
2024-03-29 17:15:57 -0500

    add user data script

--- practice/main.tf
+++ practice/main.tf
@@ -19,8 +19,8 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
     ingress{
-      from_port   = 8080
-      to_port     = 8080
+      from_port   = var.server_port
+      to_port     = var.server_port
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
