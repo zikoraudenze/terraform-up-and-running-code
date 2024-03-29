ebcb6ca2f5eaf41ac18d52c62f089441e29e865c
zikoraudenze <zikoraudenze@gmail.com>
2024-03-29 18:17:55 -0500

    add user data script

--- practice/main.tf
+++ practice/main.tf
@@ -1,19 +1,13 @@
-resource "aws_instance" "example" {
-    ami = "ami-0fb653ca2d3203ac1"
+resource "aws-launch_configuration" "example" {
+    image_id = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p ${var.server_port} &
-                EOF
-
-    user_data_replace_on_change = true
-
-    tags = {
-        Name = "terraform-example"
-    }   
+                EOF 
 }
 
 resource "aws_security_group" "instance" {
