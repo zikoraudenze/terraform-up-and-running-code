d28c05f175195bef6605c22cba3176ab53619480
zikoraudenze <zikoraudenze@gmail.com>
2024-03-29 17:24:39 -0500

    add user data script

--- practice/main.tf
+++ practice/main.tf
@@ -35,3 +35,8 @@ variable "server_port" {
     type        = number  
     default     = 8080
 }
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "The public IP address of the web server"
+}
