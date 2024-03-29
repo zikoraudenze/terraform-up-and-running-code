d53d2c1fad40198a5366c8ace93491d6d8e1a2b8
zikoraudenze <zikoraudenze@gmail.com>
2024-03-29 17:17:16 -0500

    add user data script

--- practice/main.tf
+++ practice/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
     user_data_replace_on_change = true
