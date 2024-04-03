cc720d9b8ad7865a9bd65e3d1abef334b243e98d
zikoraudenze <zikoraudenze@gmail.com>
2024-04-02 22:15:45 -0500

    add user data script

--- practice/main.tf
+++ practice/main.tf
@@ -32,6 +32,9 @@ resource "aws_autoscaling_group" "example" {
     launch_configuration = aws_launch_configuration.example.name
     vpc_zone_identifier = data.aws_subnets.default.ids
 
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
     min_size = 2
     max_size = 10
 
@@ -116,5 +119,37 @@ resource "aws_security_group" "alb" {
     to_port   = 0
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_subnets.default
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws.lb.target_group_asg.arn
   }
 }
